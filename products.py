3dd9a9ad2a339aff47609de39d1a99a0d2914d65
sabalyshen <tedoldhead@gmail.com>
2021-07-24 22:28:48 +0800

    add_forloop

--- products.py
+++ products.py
@@ -1,4 +1,5 @@
 #二維清單(2 dimensional): 清單中的清單
+"""
 products = []
 while True:
     name = input('請輸入商品名稱: ')
@@ -19,13 +20,20 @@ print(products)
 products[0][0]
 #第一個[0]就是大清單的第0格
 #第二個[0]就是大清單中的小清單的第0格
+"""
 
 #final
-game = []
+games = []
 while True:
 	game_name = input('請輸入遊戲名稱: ')
 	if game_name == 'q':
 		break
 	game_cost = input('請輸入遊戲價格: ')
-	game.append([game_name, game_cost])
-print(game)
+	games.append([game_name, game_cost])
+print(games)
+
+for g in games:
+	print(g) # 依序印出大清單(games)的東西
+	print(g[0]) # 依序印出大清單(games)中的第一個小清單
+for c in game_cost:
+	print(c) #for loot 走到最後的值
