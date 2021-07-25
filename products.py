fc9173a2025b10603bdd7ef789e3f1d32863ec61
sabalyshen <tedoldhead@gmail.com>
2021-07-25 16:55:13 +0800

    add_encoding

--- products.py
+++ products.py
@@ -29,21 +29,25 @@ while True:
 	if game_name == 'q':
 		break
 	game_cost = input('請輸入遊戲價格: ')
+	game_cost = int(game_cost)
 	games.append([game_name, game_cost])
 print(games)
 
 for g in games:
 	print(g) # 依序印出大清單(games)的東西
 	print(g[0]) # 依序印出大清單(games)中的第一個小清單
+"""
 for c in game_cost:
 	print(c) #for loot 走到最後的值
+"""
 
 # 字串可以+-*/
 #'abc' + '123' = 'abc123'
 #'abc' * 3 = 'abcabcabc'
 
-with open('products.csv', 'w') as f:
+with open('products.csv', 'w', encoding = 'utf-8') as f:
 # csv 可用excel打開，多個清單好用
+	f.write('遊戲, 價格\n') #加入欄位名稱
 	for g in games:
-		f.write(g[0] + ',' + g[1] + '\n')
+		f.write(g[0] + ',' + str(g[1]) + '\n') #str = string 數字改為字串字串
 # csv 每一格格子用 ',' 區隔
